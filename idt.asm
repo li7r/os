2e933b0f7b6dff092d4ef2d1ae8b3a03688d41c8
li7r <68507440+li7r@users.noreply.github.com>
2020-07-22 03:51:18 +0300

    Update idt.asm

--- idt.asm
+++ idt.asm
@@ -3,22 +3,26 @@ idt_desc:
 	dw 4095
 	dq _idt
 %macro PUSHALL 0
-push rax
-push rcx
-push rdx
-push r8
-push r9
-push r10
+push dx
+push ax
+push cx
+push di
+push si
 push r11
+push r10
+push r9
+push r8 
 %endmacro
 %macro POPALL 0
-push r11
-push r10
-push r9
-push r8
-push rdx
-push rcx
-push rax
+pop r8
+pop r9
+pop r10
+pop r11
+pop si
+pop di
+pop cx
+pop ax
+pop dx
 %endmacro
 extern isr1_handler
 isr1:
