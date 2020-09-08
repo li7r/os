5857d05a3bfc8f7911b187d86d6ddde490e965f9
John Hammond <68507440+li7r@users.noreply.github.com>
2020-09-08 07:54:37 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,7 @@
 nasm -f elf64  a.asm
 nasm -f elf64  b.asm
-#nasm -f elf64  idt.asm
+#ncopile lua here ! 
+#wget https://lua.org/ftp/lua-5.4.0.tar.gz
 gcc -m64 -ffreestanding -mno-red-zone -c main.cpp -o c.o 
 ld --nmagic --output=kernel.bin --script=link.ld a.o b.o c.o
 sudo mv kernel.bin isofiles/boot/
